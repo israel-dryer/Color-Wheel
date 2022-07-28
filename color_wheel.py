83bc1b4349ae0bbabca1618176c8eafb91c882bd
kanta <101532773+kantas-spike@users.noreply.github.com>
2022-07-28 10:26:58 +0900

    style: formatterで整形

--- color_wheel.py
+++ color_wheel.py
@@ -10,6 +10,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Colorwheel")
 
+
 def on_mouse_drag(event):
     """Mouse movement callback"""
     # get mouse coordinates
@@ -20,7 +21,7 @@ def on_mouse_drag(event):
     canvas.delete("all")
     canvas.create_image(365, 365, image=wheel)
     canvas.create_image(x, y, image=target)
-    
+
     # get rgb color from pixel location in image
     rgb_color = wheel.get(x, y)
 
@@ -29,20 +30,20 @@ def on_mouse_drag(event):
 
     # adjust the label background color and text
     color_label.set(hex_color + "\nrgb({},{},{})".format(*rgb_color))
-    color_select['bg'] = hex_color
-    
-    
+    color_select["bg"] = hex_color
+
+
 canvas = tk.Canvas(root, height=730, width=730)
 canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
 canvas.bind("<B1-Motion>", on_mouse_drag)
 
 color_label = tk.StringVar()
-color_label.set('#FFFFFF\nrgb(255,255,255)')
-color_select = tk.Label(root, textvariable=color_label, bg='white', width=20, font=('Arial', 20, 'bold'))
+color_label.set("#FFFFFF\nrgb(255,255,255)")
+color_select = tk.Label(root, textvariable=color_label, bg="white", width=20, font=("Arial", 20, "bold"))
 color_select.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
 
-wheel = tk.PhotoImage(file='color_wheel.png')
-target = tk.PhotoImage(file='target.png')
+wheel = tk.PhotoImage(file="color_wheel.png")
+target = tk.PhotoImage(file="target.png")
 
 canvas.create_image(365, 365, image=wheel)
 canvas.create_image(365, 365, image=target)
